fb5ad1401cfca8c8fea1a4db1a905894ff843691
Russell Heimlich <kingkool68@gmail.com>
2017-09-15 01:22:22 -0400

    Initial Commit

---
+++ feed2js.php
@@ -0,0 +1,49 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 1:17:32 Mar 24, 2012 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 2:15:45 Aug 16, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+feed2js_ck = true;
+
+document.write('<div class="rss-box">');
+document.write('<p class="rss-title"><a class="rss-title" href="/web/20120324011732/http://www.last.fm/user/kingkool68" target="_self">kingkool68\'s Recently Played Tracks</a><br /><span class="rss-item">Last 10 tracks submitted to Last.fm</span></p>');
+document.write('<ul class="rss-items">');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Deltron+3030/_/Things+You+Can+Do" title="/web/20120324011732/http://www.last.fm/music/Deltron+3030..." target="_self">Deltron 3030 ? Things You Can Do</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 02:42:53 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/We+Were+Promised+Jetpacks/_/It%27s+Thunder+And+It%27s+Lightning" title="/web/20120324011732/http://www.last.fm/music/We+Were+Promised+Jetpacks..." target="_self">We Were Promised Jetpacks ? It\'s Thunder And It\'s Lightning</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 02:24:23 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Killarmy/_/Dress+to+Kill" title="/web/20120324011732/http://www.last.fm/music/Killarmy..." target="_self">Killarmy ? Dress to Kill</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 02:18:54 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Mew/_/Comforting+Sounds+%28Album+Version%29" title="/web/20120324011732/http://www.last.fm/music/Mew..." target="_self">Mew ? Comforting Sounds (Album Version)</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 02:11:05 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey/_/Blue+Jeans+%28PatrickReza+Dubstep+Remix%29" title="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey..." target="_self">Lana Del Rey ? Blue Jeans (PatrickReza Dubstep Remix)</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 01:55:27 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey/_/Diet+Mountain+Dew+%28Tommy+Noble+Remix%29" title="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey..." target="_self">Lana Del Rey ? Diet Mountain Dew (Tommy Noble Remix)</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 01:49:36 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey/_/Yayo" title="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey..." target="_self">Lana Del Rey ? Yayo</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 01:44:46 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey/_/Video+Games+%28BALAM+ACAB+Remix%29" title="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey..." target="_self">Lana Del Rey ? Video Games (BALAM ACAB Remix)</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 01:37:17 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey/_/Blue+Jeans+%28PatrickReza+Dubstep+Remix%29" title="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey..." target="_self">Lana Del Rey ? Blue Jeans (PatrickReza Dubstep Remix)</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 12:42:15 pm</span><br />');
+document.write('</li>');
+document.write('<li class="rss-item"><a class="rss-item" href="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey/_/Video+Games" title="/web/20120324011732/http://www.last.fm/music/Lana+Del+Rey..." target="_self">Lana Del Rey ? Video Games</a><br />');
+document.write('<span class="rss-date">published on March 23, 2012 12:37:58 pm</span><br />');
+document.write('</li>');
+document.write('</ul></div>');
